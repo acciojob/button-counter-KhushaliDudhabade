3c1b9c36963b228d7bc7506e538292c006bbb23c
Khushali Dudhabade <khushalidudhabade27@gmail.com>
2024-07-17 12:56:13 +0530

    completed counter

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[counter,setCounter]=useState(0)
+
+  const increment=()=>{
+    setCounter(counter+1);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked <span>{counter}</span> times</p>
+
+        <button onClick={increment}>Click me</button>
     </div>
   )
 }
